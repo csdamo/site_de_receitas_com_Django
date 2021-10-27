6662d086b83c3f10c30fcad207d9ee2e1ac260ef
csdamo <csdamo.cx@gmail.com>
2021-10-27 10:14:51 -0300

    Criando paginação na página principal

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
-from .models import Receita
+from receitas.models import Receita
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
+    paginator = Paginator(receitas, 3)
+    page = request.GET.get('page')
+    receitas_por_pagina = paginator.get_page(page)
     dados = {
-        'receitas': receitas
+        'receitas': receitas_por_pagina
     }
     return render(request, 'receitas/index.html', dados)
 
@@ -16,19 +20,6 @@ def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
     receita_a_exibir = {'receita': receita}
     return render(request, 'receitas/receita.html', receita_a_exibir)
-
-
-def buscar(request):
-    lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
-    if 'buscar' in request.GET:
-        nome_a_buscar = request.GET['buscar']
-        if buscar:
-            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
-
-        dados = {
-            'receitas': lista_receitas
-        }
-    return render(request, 'receitas/buscar.html', dados)
 
 
 def cria_receita(request):
